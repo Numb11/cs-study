d129a009a61f139512f3562e949444ef10ff4293
Soup <87945079+Numb11@users.noreply.github.com>
2025-02-11 15:53:55 +0000

    Commented and error catching

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from collections import defaultdict
 
 def findAnagrams(file_path):
+    try:
         anagramsDict = defaultdict(list) 
-        with open(file_path) as file: 
+        with open(file_path) as file: #Open the text file
 
             for line in file:
-                word = line.strip().lower() 
-                if word.isalpha(): 
-                    sortedWord = "".join(sorted(word)) 
-                    anagramsDict[sortedWord].append(word) 
+                word = line.strip().lower() #Strip lines removing newline characters, enforce lower to ensure matches can be made if the incorrect data is input
+                if word.isalpha(): #Ignore digits as these are of incorrect type
+                    sortedWord = "".join(sorted(word)) #Sort the word alphabetically then return to string using join method
+                    anagramsDict[sortedWord].append(word) #Add sorted word as a key if possible, append the word to thr key-value list dicts
         
-        return anagramsDict 
+        return anagramsDict # Return the dictionary/hashtable object
+    
+    except FileNotFoundError as e:
+        print(f"File not found {e}, check file/path")
+        return None
+    except PermissionError as e:
+        print(f"File operation not permitted {e}, check file/path")
+        return None
     
 
-anagramDict = findAnagrams("words.txt") 
+anagramDict = findAnagrams("words.txt") #Calling the function initialised and defined above with file path string as arg
 
 if anagramDict:
 
